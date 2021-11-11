9010499e1c9765bcac002bc814a0ad6bb9588872
Code-Warlock <dessymichaeltgp123@gmail.com>
2021-11-11 13:39:14 +0100

    Add Auction File and more_func file

--- conditional.py
+++
@@ -1,7 +0,0 @@
-age = int(input("Enter your age : "))
-if age > 18:
-    print("Elligible to vote.")
-elif age == 18:
-    print("Wait after 4 years.")
-else:
-    print("Too young to vote.")
--- tuples.py
+++
@@ -1,5 +0,0 @@
-males  = input("Enter your name : "),input("Enter your name : "),input("Enter your name : "),input("Enter your name : ")
-# males2 = males
-
-male1,*rest,male2= males
-print(males)
