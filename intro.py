4a046ba28a2e488bc347ea1859cc6e0252aa6094
Code-Warlock <dessymichaeltgp123@gmail.com>
2022-01-28 10:48:14 +0100

    Introduction to OOP

---
+++ intro.py
@@ -0,0 +1,68 @@
+""" Introduction to Object Oriented Programming in Python """
+
+# Inheritance
+# Abstraction
+# Encapsulation
+# Polymorphism
+
+
+class Human:
+  
+  def __init__(self , name ,skin_color, legs = 2  , hands=2):
+    self.name = name
+    self.skin_color = skin_color
+    self.hands = hands
+    self.legs = legs
+
+
+  def say_name(self):
+    return f"Hello my name is {self.name}"
+
+
+human = Human("Desmond" , "black")
+human1 = Human("Samuel","black")
+print(human1.legs)
+
+# human1 = Human()
+# des = Human()
+
+# print(human1.say_name())
+# print(des.say_name())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def hello():
+#   return 'Hello World'
+
+# if __name__ != '__main__':
+#   print("You didn't run this directly!")
---
+++ intro_2.py
@@ -0,0 +1,7 @@
+# import intro
+
+# def main():
+#   return intro.hello()
+
+# if __name__ == '__main__':
+#   print(main())
